7bbc9b0414cf919c6bb4618183627a026ca60ec8
oyama kentaro <kota-k@live.jp>
2016-04-15 19:49:07 +0900

    カメラ追従追加

--- Assets/Script/Common/CameraController2.cs
+++ Assets/Script/Common/CameraController2.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class CameraController2 : MonoBehaviour {
-    public float m_Height = 15.0f;  // カメラの高さ.
-    public float m_playDistanceRatioWidht = 0.5f; // x遊びの画面割合.
-    public float m_playDistanceRatioHeight = 0.5f;  // x遊びの画面割合.
+    static float m_criteriaHeight = 15.0f; // カメラの高さ基準.
+    public float m_Height = m_criteriaHeight;  // カメラの高さ.
+    public float m_playDistanceRatio = 0.5f; // 遊びの画面割合.
     public float m_offsetXRatio = 0.0f; // オフセット位置の画面割合.
     public float m_offsetYRatio = 0.0f; // オフセット位置の画面割合.
     public GameObject m_traceTarget = null; // トレス対象.
+    public float m_speed = 1.0f;
     Camera m_controllCamera = null; // カメラ.
+    float m_screenSizeMin = 100;
+
+    Vector3 m_work = new Vector3();
 
     // Use this for initialization
     void Start () {
         m_controllCamera = GetComponent<Camera>();
+        m_screenSizeMin = Mathf.Min(Screen.width, Screen.height);
     }
 
     // Update is called once per frame
@@ -23,19 +28,23 @@ public class CameraController2 : MonoBehaviour {
         }
 
         Vector3 pos = transform.position;
-        Vector3 posScreenPos = m_controllCamera.WorldToScreenPoint(pos);
         Vector3 targetPos = m_traceTarget.transform.position;
         Vector3 targetScreenPos = m_controllCamera.WorldToScreenPoint(targetPos);
-        float playWidht = Screen.width * m_playDistanceRatioWidht;
-        float playHeight = Screen.height * m_playDistanceRatioHeight;
-        float screenWidhtHalf = Screen.width * 0.5f;
-        float screenHeightHalf = Screen.height * 0.5f;
-        
-        //if () {
-          //  pos.x = targetPos.x;
-        //}
+        targetScreenPos.z = 0.0f;
+        m_work.x = Screen.width * 0.5f;
+        m_work.y = Screen.height * 0.5f;
+        m_work.z = 0.0f;
+
+        float playDistance = m_screenSizeMin * m_playDistanceRatio;
+        if (Vector3.Distance(targetScreenPos, m_work) > playDistance)
+        {
+            Vector3 cameraWorldPos = m_controllCamera.ScreenToWorldPoint(targetScreenPos);
+            cameraWorldPos.y = targetPos.y;
+            float distance = Vector3.Distance(cameraWorldPos, targetPos);
+            pos += (targetPos - cameraWorldPos).normalized * m_speed * (m_Height / m_criteriaHeight) * Time.deltaTime;
+        }
+
         pos.y = m_Height;
-        //pos.z = targetPos.z;
         transform.position = pos;
     }
 }
