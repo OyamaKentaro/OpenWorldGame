ee2a487a7fa43650cf822c2f2578869e4e95b768
oyama kentaro <kota-k@live.jp>
2016-04-15 23:42:09 +0900

    テスト的なブースター挙動script追加　物理マテリアル変更

---
+++ Assets/Script/Common/TestBooster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBooster : MonoBehaviour {
+    Rigidbody m_rigidBody = null;
+    public Vector3 power = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (m_rigidBody == null)
+        {
+            return;
+        }
+
+        m_rigidBody.AddForce(power);
+    }
+}
